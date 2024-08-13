a8861259e255affdb2900789eed4a1b88d72e42c
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-08-13 03:38:58 -0600

    syncaroni

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,20 +6,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    // Proxy configuration to forward requests to the server running on port 3001.
     proxy: {
-      "/graphql": {
+      "/api": {
         target: "http://localhost:3001",
         secure: false,
         changeOrigin: true,
       },
     },
-  },
-  build: {
-    outDir: "dist", // Ensure the build output goes to the correct directory
-  },
-  test: {
-    globals: true,
-    environment: "happy-dom",
   },
 });
